dc83b21de82a53c779d79ef102a28aea5646f5fa
Aldrich Lapid <alapid@LABA.PSWAS.LOCAL>
2023-05-02 15:55:32 -0700

    files completed

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
 import HelloWorld from './components/helloWorld';
-import ColorChanger from './components/colorChanger'
+import ColorChanger from './components/colorChanger';
+import ContactForm from './components/contactForm';
 
 function App() {
   return (
     <div className="App">
       <HelloWorld/>
       <ColorChanger/>
+      <ContactForm/>
     </div>
     
   );
